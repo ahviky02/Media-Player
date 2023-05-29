25c050561b0f77f1e8514c71c178a17c5ab16327
Avdhesh Kumar Yadav <cpaavdhesh@gmail.com>
2023-05-30 00:16:03 +0530

    duration functionality add

--- api/upload_music.php
+++ api/upload_music.php
@@ -1,27 +1,32 @@
 <?php
 session_start(); // Start the session
 require_once 'dbcon.php';
+require_once '../Library/getid3/getid3.php';
 
 if (isset($_POST['upload_music'])) {
     $targetDirectory = "../uploads/";
     $uploadedFile = $_FILES['file']['name'];
     $targetFile = $targetDirectory . basename($uploadedFile);
 
+    $getID3 = new getID3();
+
+    // Analyze the audio file
+    $fileInfo = $getID3->analyze($_FILES['file']['tmp_name']);
+
     if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) {
         $pre_name = $_FILES['file']['name'];
         $status = 1;
         $music_name = $_POST['music_name'];
+        $duration = $fileInfo['playtime_seconds'];
         $created_at = date("Y-m-d H:i:s");
 
-        $sql_query = "INSERT INTO music (music_name,pre_name, status, created_at) VALUES ('$music_name','$pre_name', '$status', '$created_at')";
+        $sql_query = "INSERT INTO music (music_name, pre_name, status, duration, created_at) VALUES ('$music_name', '$pre_name', '$status', '$duration', '$created_at')";
         $conn->query($sql_query);
 
         $_SESSION['Music_Upload'] = "File uploaded successfully.";
     } else {
         $_SESSION['Music_Upload'] = "Error uploading the file.";
     }
-
-
 
     // Redirect to the current page to display the message
     header("Location: ../upload.php");
