e7e9be0e19732eb7adb11ae4d1a362fd8781bbdf
Avdhesh Kumar Yadav <cpaavdhesh@gmail.com>
2023-05-31 03:53:23 +0530

    css responsive

--- index.php
+++ index.php
@@ -3,47 +3,21 @@ include('header.php');
 require_once './api/dbcon.php';
 
 ?>
+<div class="part-1">
 
-<div class="row">
-    <div>
-        <?php
-        // Define the active page variable based on the current page
-        $active_page = basename($_SERVER['PHP_SELF'], ".php");
-        // Include the navbar.php file
-        include('side-bar.php');
-        ?>
-    </div>
-
-    <div class="main-content col-md-9 col-sm-7">
-
-
-        <div class="container m-5">
-            <h2>Home</h2>
-
-
-            <!-- <?php
-
-                    ?>
-            <div class="row">
-                <div class="col-sm-6 col-md-4">
-
-                    <li><?= htmlspecialchars($music['pre_name']) ?></li>
-                    <?php
-                    ?> -->
-
-
-            <div class="table-container" style="width:auto; height:70vh; overflow: scroll;">
-
-            </div>
-        </div>
-
-
-    </div>
-</div>
+    <?php
+    // Define the active page variable based on the current page
+    $active_page = basename($_SERVER['PHP_SELF'], ".php");
+    // Include the navbar.php file
+    include('side-bar.php');
+    ?>
 
 </div>
-</div>
+<div class="part-2">
 
+    <h2>Home</h2>
+
+</div>
 </body>
 
 </html>
--- upload.php
+++ upload.php
@@ -4,57 +4,39 @@ session_start(); // Start the session
 require_once './api/dbcon.php';
 include('header.php');
 ?>
+<div class="part-1">
 
+    <?php
+    // Define the active page variable based on the current page
+    $active_page = basename($_SERVER['PHP_SELF'], ".php");
+    // Include the navbar.php file
+    include('side-bar.php');
+    ?>
 
-<div class="row">
+</div>
+<div class="part-2">
+    <h2>Upload Audio </h2>
+    <form action="./api/upload_music.php" method="post" enctype="multipart/form-data">
 
+        <div class="form-1">
 
-    <div>
-        <?php
-        // Define the active page variable based on the current page
-        $active_page = basename($_SERVER['PHP_SELF'], ".php");
-        // Include the navbar.php file
-        include('side-bar.php');
+            <div class="control-form">
+                <label for="music_name">Audio Name</label>
+                <input type="text" name="music_name">
+            </div>
 
-        ?>
+            <div class="control-form">
+                <label class="form-label">Upload Music</label>
+                <input type="file" accept="audio/mp3, audio/wav" class="form-control" id="f" name="file">
+            </div>
 
+            <div class="control-form">
+                <input name="upload_music" type="submit" />
+            </div>
+    </form>
+</div>
 
-    </div>
-
-    <div class="main-content col-md-9 col-sm-7">
-
-
-
-        <div class="upload">
-
-            <form action="./api/upload_music.php" method="post" enctype="multipart/form-data">
-                <h2 style="text-align: center;">Audio File Upload</h2>
-
-                <div class="mb-3">
-                    <span class="text-success"><?php
-                                                // Display the message if it exists
-                                                if (isset($_SESSION['Music_Upload'])) {
-                                                    echo $_SESSION['Music_Upload'];
-                                                    unset($_SESSION['Music_Upload']); // Clear the message
-                                                }
-                                                ?></span>
-                </div>
-
-                <div class="mb-3">
-                    <label for="music_name" class="form-label">Audio Name</label>
-                    <input type="text" name="music_name" class="form-control">
-                </div>
-
-                <div class="mb-3">
-                    <label class="form-label">Upload Music</label>
-                    <input type="file" accept="audio/mp3, audio/wav" class="form-control" id="f" name="file">
-                </div>
-                <br>
-                <button class="btn btn-success" name="upload_music" type="submit">Submit</button>
-            </form>
-        </div>
-
-    </div>
+</div>
 </div>
 
 
