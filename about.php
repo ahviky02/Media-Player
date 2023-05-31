750fd020312df34df78deb1298e4e7e36fa3ceb8
Avdhesh Kumar Yadav <cpaavdhesh@gmail.com>
2023-06-01 03:00:04 +0530

    about us page add

--- about.php
+++ about.php
@@ -1,58 +1,36 @@
 <?php
 include('header.php');
 ?>
+<div class="part-1">
 
-<div class="row">
-    <div>
-        <?php
-        // Define the active page variable based on the current page
-        $active_page = basename($_SERVER['PHP_SELF'], ".php");
-        // Include the navbar.php file
-        include('side-bar.php');
-        ?>
-    </div>
+    <?php
+    // Define the active page variable based on the current page
+    $active_page = basename($_SERVER['PHP_SELF'], ".php");
+    // Include the navbar.php file
+    include('side-bar.php');
+    ?>
 
-    <div class="main-content col-md-9 col-sm-7">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-        <script>
-        // JavaScript variable
-        var myVariable = "Hello PHP!";
-
-        // Make an Ajax request
-        $.ajax({
-            url: 'about.php', // Replace with the actual path to your PHP script
-            type: 'POST',
-            data: {
-                myVariable: myVariable
-            },
-            success: function(response) {
-                // Handle the response from PHP
-                console.log(response);
-            },
-            error: function(xhr, status, error) {
-                // Handle error
-                console.log(error);
-            }
-        });
-        </script>
-
-
-
-        <?php
-        // Retrieve the variable from the Ajax request
-        $myVariable = $_POST['myVariable'];
-
-        // Use the variable in your PHP code
-        echo "Received variable: " . $myVariable;
-        ?>
-
-
-
-
-    </div>
 </div>
 
-</body>
+<div class="part-2">
 
-</html>
+    <h2>About Us</h2>
+
+    <p>
+    <pre>
+        Hello Guys,
+        
+                My Name is Ahvi and this website is created by me. This website is a media-player and name is My-Player.
+         In this Website using bootstrap icon and php libraries and some javaScript.This is made for Entertainment and 
+         Knowledge purpuse. 
+
+
+
+
+                                                                     <h7>Thank You !
+                                                                 Have a Nice day!</h7>
+        </pre>
+
+    </p>
+
+</div>
--- side-bar.php
+++ side-bar.php
@@ -1,39 +1,46 @@
 <div class="side-bar">
     <ul>
-        <li class="nav-item"><a href="index.php" <?php if ($active_page === 'index') {
-                                                        echo
-                                                        'class="nav-link active"';
-                                                    } else {
-                                                        echo 'class="nav-link link-dark"';
-                                                    }  ?>><i class="bi bi-house pr-2"></i> Home</a></li>
+        <li><a href="index.php" <?php if ($active_page === 'index') {
+                                    echo
+                                    'class="active"';
+                                } else {
+                                    echo 'class="link-dark"';
+                                }  ?>><i class="bi bi-house pr-2"></i> Home</a></li>
 
         <li><a href="music.php" <?php if ($active_page === 'music') {
                                     echo
-                                    'class="nav-link active"';
+                                    'class="active"';
                                 } else {
-                                    echo 'class="nav-link link-dark"';
+                                    echo 'class="link-dark"';
                                 } ?>><i class="bi bi-music-note-list pr-2"></i></i>Music Library</a></li>
 
         <li><a href="video.php" <?php if ($active_page === 'video') {
                                     echo
-                                    'class="nav-link active"';
+                                    'class="active"';
                                 } else {
-                                    echo 'class="nav-link link-dark"';
+                                    echo 'class="link-dark"';
                                 } ?>><i class="bi bi-camera-video pr-2"></i>Video Library</a></li>
 
         <li><a href="upload.php" <?php if ($active_page === 'upload') {
                                         echo
-                                        'class="nav-link active"';
+                                        'class="active"';
                                     } else {
-                                        echo 'class="nav-link link-dark"';
+                                        echo 'class="link-dark"';
                                     } ?>><i class="bi bi-upload pr-2"></i>Upload Audio</a></li>
 
         <li><a href="upload-video.php" <?php if ($active_page === 'upload-video') {
                                             echo
-                                            'class="nav-link active"';
+                                            'class="active"';
                                         } else {
-                                            echo 'class="nav-link link-dark"';
+                                            echo 'class="link-dark"';
                                         } ?>><i class="bi bi-upload pr-2"></i>Upload Video</a></li>
+
+        <li><a href="about.php" <?php if ($active_page === 'about') {
+                                    echo
+                                    'class="active"';
+                                } else {
+                                    echo 'class="link-dark"';
+                                }  ?>><i class="bi bi-person-circle"></i> About Us</a></li>
 
     </ul>
 </div>
